e37800f531aeaf6db639db7bac1847628bafa9ce
sergio.fdz.gar@gmail.com <sergio.fdz.gar@gmail.com@b0b897f5-2973-26d8-3355-3fdc1e9af6a3>
2011-04-11 00:16:00 +0000

    Comienzo de la parte "Acerca de"

---
+++ UNED-UD2-Practica2/src/com/uned/ud2/practica2/AboutActivity.java
@@ -0,0 +1,17 @@
+package com.uned.ud2.practica2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutActivity extends Activity {
+
+	public AboutActivity() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+	}
+}
