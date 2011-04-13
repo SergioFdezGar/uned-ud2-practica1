47608c17dd1d2aa02e00e4274a0a039c9da0cb47
sergio.fdz.gar@gmail.com <sergio.fdz.gar@gmail.com@b0b897f5-2973-26d8-3355-3fdc1e9af6a3>
2011-04-13 20:31:34 +0000

    Introducido el zoom por defecto y centrado de todos los puntos en el mapa, optimizado para mostrar sólo un punto en el mapa.

--- UNED-UD2-Practica2/src/com/uned/ud2/practica2/CustomItemizedOverlay.java
+++ UNED-UD2-Practica2/src/com/uned/ud2/practica2/CustomItemizedOverlay.java
@@ -1,36 +1,54 @@
 package com.uned.ud2.practica2;
 
-import java.util.List;
-
 import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
-import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
-import com.google.android.maps.Overlay;
 import com.google.android.maps.OverlayItem;
 
 public class CustomItemizedOverlay extends MapActivity {
 
-	LinearLayout llMapa;
-	MapView mvMapa;
-	List<Overlay> mapOverlays;
-	Drawable drawable;
-	MyMapActivity itemizedOverlay;
-	MapController ctrlmapa;
+	private MapView mvMapa;
+	private Drawable drawable;
+	private MyMapActivity itemizedOverlay;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mapa);
+		
 		mvMapa=(MapView)findViewById(R.id.mv_mapa);
 		mvMapa.setBuiltInZoomControls(true);
 		
+		modoMapa();
+		
+		drawable = this.getResources().getDrawable(R.drawable.entrada);
+		itemizedOverlay = new MyMapActivity(drawable, this);
+		mvMapa.getOverlays().add(itemizedOverlay);
+		
+		Bundle bundle=this.getIntent().getExtras();
+		
+		GeoPoint point = new GeoPoint(bundle.getInt("latitud"), bundle.getInt("longitud"));
+		OverlayItem overlayitem = new OverlayItem(point, bundle.getString("mision").toString(), bundle.getString("direccion").toString());
+		itemizedOverlay.addOverlay(overlayitem);
+		zoomLocalizacion(point);
+		
+	}
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+	
+	private void modoMapa(){
 		SharedPreferences preferencias = PreferenceManager.getDefaultSharedPreferences(this);
 		
 		switch(Integer.parseInt(preferencias.getString("view_map", ""))){
@@ -43,94 +61,22 @@ public class CustomItemizedOverlay extends MapActivity {
 				mvMapa.setSatellite(true);
 				mvMapa.setTraffic(false);
 			break;
+			
+			default:
+				mvMapa.setSatellite(false);
+				mvMapa.setTraffic(true);
 
 		}
-		
-		mapOverlays = mvMapa.getOverlays();
-		drawable = this.getResources().getDrawable(R.drawable.entrada);
-		itemizedOverlay = new MyMapActivity(drawable, this);
-		
-		Bundle bundle=this.getIntent().getExtras();
-		
-		GeoPoint point = new GeoPoint(bundle.getInt("latitud"), bundle.getInt("longitud"));
-		OverlayItem overlayitem = new OverlayItem(point, bundle.getString("mision").toString(), bundle.getString("direccion").toString());
-		//ctrlmapa.setCenter(point);
-		itemizedOverlay.addOverlay(overlayitem);
-		mapOverlays.add(itemizedOverlay);
-		
-		
 	}
 	
+	private void zoomLocalizacion(GeoPoint point){
+        if(point != null) {
+        	mvMapa.getController().animateTo(point);
+        	mvMapa.getController().setZoom(18);
+        }
+        else {
+                Toast.makeText(this, "No se ha podido determinar la posición", Toast.LENGTH_SHORT).show();
+        }
 
-	@Override
-	protected boolean isRouteDisplayed() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	protected void onDestroy(){
-		super.onDestroy();
-		mapOverlays = null;
-		drawable = null;
-		itemizedOverlay = null;
-		
-		GeoPoint point = new GeoPoint(0, 0);
-		
-		
 	}
 }
-
-
-/*
-package com.uned.ud2.practica2;
-
-
-import java.util.ArrayList;
-
-import android.app.AlertDialog;
-import android.content.Context;
-import android.graphics.drawable.Drawable;
-
-import com.google.android.maps.ItemizedOverlay;
-import com.google.android.maps.OverlayItem;
-
-public class CustomItemizedOverlay extends ItemizedOverlay{
-
-	private ArrayList<OverlayItem> oliItems= new ArrayList<OverlayItem>();
-	private Context context;
-	
-	public CustomItemizedOverlay(Drawable defaultMarker, Context context) {
-		super(boundCenterBottom(defaultMarker));//Siguiendo el ejemplo de la documentacion
-		this.context=context;
-		
-	}
-
-	@Override
-	protected OverlayItem createItem(int i) {
-		// TODO Auto-generated method stub
-		return oliItems.get(i);
-	}
-
-	@Override
-	public int size() {
-		// TODO Auto-generated method stub
-		return oliItems.size();
-	}
-
-	public void addOverlay(OverlayItem overlay){
-		oliItems.add(overlay);
-		populate();
-	}
-	
-	protected boolean onTap(int index){
-		OverlayItem item = oliItems.get(index);
-	    AlertDialog.Builder dialog = new AlertDialog.Builder(this.context);
-	    dialog.setTitle(item.getTitle());
-	    dialog.setMessage(item.getSnippet());
-	    dialog.show();
-	    return true;
-	}
-	
-}
-*/
--- UNED-UD2-Practica2/src/com/uned/ud2/practica2/MyMapActivity.java
+++ UNED-UD2-Practica2/src/com/uned/ud2/practica2/MyMapActivity.java
@@ -1,8 +1,5 @@
 package com.uned.ud2.practica2;
 
-
-import java.util.ArrayList;
-
 import android.app.AlertDialog;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
@@ -12,113 +9,36 @@ import com.google.android.maps.OverlayItem;
 
 public class MyMapActivity extends ItemizedOverlay{
 
-	private ArrayList<OverlayItem> oliItems= new ArrayList<OverlayItem>();
+	private OverlayItem oliItems;
 	private Context context;
 	
 	public MyMapActivity(Drawable defaultMarker, Context context) {
-		super(boundCenterBottom(defaultMarker));//Siguiendo el ejemplo de la documentacion
-		this.context=context;
-		
+		super(boundCenterBottom(defaultMarker));
+		this.context=context;	
 	}
 
 	@Override
 	protected OverlayItem createItem(int i) {
 		// TODO Auto-generated method stub
-		return oliItems.get(i);
+		return oliItems;
 	}
 
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
-		return oliItems.size();
+		return 1;
 	}
 
 	public void addOverlay(OverlayItem overlay){
-		oliItems.add(overlay);
+		oliItems=overlay;
 		populate();
 	}
 	
 	protected boolean onTap(int index){
-		OverlayItem item = oliItems.get(index);
 	    AlertDialog.Builder dialog = new AlertDialog.Builder(this.context);
-	    dialog.setTitle(item.getTitle());
-	    dialog.setMessage(item.getSnippet());
+	    dialog.setTitle(oliItems.getTitle());
+	    dialog.setMessage(oliItems.getSnippet());
 	    dialog.show();
 	    return true;
 	}
-	
 }
-/*
-package com.uned.ud2.practica2;
-
-import java.util.List;
-
-import android.content.SharedPreferences;
-import android.graphics.drawable.Drawable;
-import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.widget.LinearLayout;
-
-import com.google.android.maps.GeoPoint;
-import com.google.android.maps.MapActivity;
-import com.google.android.maps.MapView;
-import com.google.android.maps.Overlay;
-import com.google.android.maps.OverlayItem;
-
-public class MyMapActivity extends MapActivity {
-
-	LinearLayout llMapa;
-	MapView mvMapa;
-	List<Overlay> mapOverlays;
-	Drawable drawable;
-	CustomItemizedOverlay itemizedOverlay;
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState){
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.mapa);
-		mvMapa=(MapView)findViewById(R.id.mv_mapa);
-		mvMapa.setBuiltInZoomControls(true);
-		
-		SharedPreferences preferencias = PreferenceManager.getDefaultSharedPreferences(this);
-		
-		switch(Integer.parseInt(preferencias.getString("view_map", ""))){
-			case 0:
-				mvMapa.setSatellite(false);
-				mvMapa.setTraffic(true);
-			break;
-			
-			case 1:
-				mvMapa.setSatellite(true);
-				mvMapa.setTraffic(false);
-			break;
-
-		}
-		
-		mapOverlays = mvMapa.getOverlays();
-		drawable = this.getResources().getDrawable(R.drawable.entrada);
-		itemizedOverlay = new CustomItemizedOverlay(drawable, this);
-		
-		Bundle bundle=this.getIntent().getExtras();
-		
-		GeoPoint point = new GeoPoint(bundle.getInt("latitud"), bundle.getInt("longitud"));
-		OverlayItem overlayitem = new OverlayItem(point, bundle.getString("mision").toString(), bundle.getString("direccion").toString());
-		
-		itemizedOverlay.addOverlay(overlayitem);
-		mapOverlays.add(itemizedOverlay);
-		
-		
-	}
-	
-	public MyMapActivity() {
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	protected boolean isRouteDisplayed() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-}
-*/
